d18e765a88826247e994f301886395ef1e45e96d
Adrian Epifanio Rodriguez <alu0101158280@ull.edu.es>
2020-11-19 11:30:46 +0000

    Practica 3 terminada

--- Práctica 3. Implementación de funciones recursivas primitivas/src/main.cpp
+++ Práctica 3. Implementación de funciones recursivas primitivas/src/main.cpp
@@ -17,7 +17,7 @@
 * @Author: Adrian Epifanio
 * @Date:   2020-11-12 20:09:53
 * @Last Modified by:   Adrian Epifanio
-* @Last Modified time: 2020-11-12 21:26:52
+* @Last Modified time: 2020-11-19 11:29:05
 */
 /*------------------  FUNCTIONS  -----------------*/
 
@@ -26,6 +26,11 @@
 /*------------------------------------------------*/
 
 int main (void) {
-	PrimitveRecursiveFunction asd;
-	std::cout << asd.pow(2,8);
+    int base, exp;
+    std::cout << std::endl << "Introduce the base of the pow number: ";
+    std::cin >> base;
+    std::cout << std::endl << "Introduce the exponent of the pow number: ";
+    std::cin >> exp;
+	PrimitveRecursiveFunction pow;
+	std::cout <<std::endl << pow.pow(base, exp) << std::endl;
 }
